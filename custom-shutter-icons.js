f003276a455b2a63f5b3cfe0cc18fb244802e0d3
Gábor Kiss <70820303+g-kiss@users.noreply.github.com>
2024-10-07 22:26:58 +0200

    Update custom-shutter-icons.js

--- custom-shutter-icons.js
+++ custom-shutter-icons.js
@@ -1,22 +1,16 @@
-console.info(
-  `%c  Custom-shutter-icons  \n%c  Version a1bb89c   `,
-  'color: orange; font-weight: bold; background: black',
-  'color: white; font-weight: bold; background: dimgray',
-);
-
 const CUSTOM_SHUTTER_ICONS_MAP = {
     
   "shutter-0":
-    "M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8",
+    "M3 4H21V8H19V20H17V8H7V20H5V8H3V49Z",
 
   "shutter-1":
-    "M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9M8",
+    "M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9Z",
     
   "shutter-2":
-    "M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9M8 12H16V14H8V12M8",
+    "M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9M8 12H16V14H8V129Z",
 
   "shutter-3":
-    "M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9M8 12H16V14H8V12M8 15H16V17H8V15M8",
+    "M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9M8 12H16V14H8V12M8 15H16V17H8V159Z",
 
   "shutter-4":
     "M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9M8 12H16V14H8V12M8 15H16V17H8V15M8 18H16V20H8V18Z",
@@ -34,12 +28,12 @@ return { path: CUSTOM_SHUTTER_ICONS_MAP[name] };
 }
 
 window.customIconsets = window.customIconsets || {};
-window.customIconsets["csi"] = getIcon;
+window.customIconsets["custom-shutter"] = getIcon;
 
 if (!window.frontendVersion || window.frontendVersion < 20200519.0) {
   // ha-iconset-svg (Up to Home Assistant 0.109):
   const iconset = document.createElement("ha-iconset-svg");
-  iconset.name = "csi";
+  iconset.name = "custom-shutter";
   iconset.size = "24";
 
   let iconsetHTML = "";
